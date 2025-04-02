1fa2244314d5402374605f1997b444097ee3a1c1
Ambadi7 <ambadikr24@gmail.com>
2025-04-02 10:07:13 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 const Navbar = () => {
   const [extendNavbar,setExtendNavbar] = useState(false)
   return (
-    <div className={`px-15 md:px-8  backdrop-blur-[2px] border-b border-fuchsia-950 w-full h-16  fixed top-0 left-0 z-100 flex justify-between md:items-center transition-all duration-500 ${extendNavbar ? "h-80" : "h-16 "}`}>
+    <div className={`px-15 md:px-8  backdrop-blur-[3px] border-b border-fuchsia-950 w-full h-16  fixed top-0 left-0 right-0 z-100 flex justify-between md:items-center transition-all duration-500 ${extendNavbar ? "h-80" : "h-16 "}`}>
         <div className={`flex h-16 items-center gap-4 font-source-code-pro text-xl text-white`}>
           <h1>Logo </h1>
           <h1>Ambadi</h1>
