400cd5d1009ebe28e27a584e3b639a0b6c3b1926
Ambadi7 <ambadikr24@gmail.com>
2025-04-21 11:26:14 +0530

    loading-screen-final

--- src/components/Loading_Screen.jsx
+++ src/components/Loading_Screen.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 import CodeLoading from './CodeLoading';
 
 const Loading_Screen = () => {
   return (
     <div className='flex justify-center items-center  h-screen w-full  text-xl'>
-        <div>
+        <motion.div 
+          initial={{ opacity :0, scale :0}}
+          animate={{opacity : [0.1,0.2,0.3,0.4,0.5,0.7] , scale : [0.1,0.2,0.4,0.6,0.8,1]}}
+
+          transition={{duration :2}}
+        >
             <CodeLoading/>
-        </div>
+        </motion.div>
     </div>
   )
 }
